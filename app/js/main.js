b28782dbf9c32306bff7324badf97680345d9f9d
dkhimich <dmitriy-works@ukr.net>
2023-12-23 12:39:36 +0300

    added products section

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,11 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 2000,
   })
+
+  $(".star").rateYo({    
+    starWidth: "17px",
+    normalFill: "#ccccce", /*цвет не закрашеной звездочки */
+    ratedFill: "#ffc35b",/*цвет закрашеной звездочки */
+    readOnly: true, /*при наводе мышки цвет звездочки не меняется */
+  });
 });
